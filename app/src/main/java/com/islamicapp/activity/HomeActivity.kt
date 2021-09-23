9df12c4344eae7e7a5d6b40511c4c4c292c7ff91
anggaps24 <anggapsps@gmail.com>
2021-09-23 18:26:28 +0700

    --> Add HomeActivity

---
+++ app/src/main/java/com/islamicapp/activity/HomeActivity.kt
@@ -0,0 +1,16 @@
+package com.islamicapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.islamicapp.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+    }
+}
--- app/src/main/java/com/islamicapp/activity/MainActivity.kt
+++ app/src/main/java/com/islamicapp/activity/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.islamicapp.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 import com.islamicapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 3000)
+
     }
 }
