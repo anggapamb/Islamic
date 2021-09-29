912e0c729900caf33910308ab544b8183eb04607
anggaps24 <anggapsps@gmail.com>
2021-09-29 14:28:10 +0700

    --> UI clear "untuk ui udah selese tpi API masi error" ~sementara project ditunda~

--- app/src/main/java/com/islamicapp/activity/HomeActivity.kt
+++ app/src/main/java/com/islamicapp/activity/HomeActivity.kt
@@ -2,15 +2,31 @@ package com.islamicapp.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import com.islamicapp.R
+import com.islamicapp.databinding.ActivityHomeBinding
+import com.islamicapp.fragment.AllMenuFragment
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
+    }
+
+    override fun onClick(p0: View?) {
+        when (p0) {
+            binding.linearMenuLainnya -> {
+                val bottomSheetAllMenu = AllMenuFragment()
+                bottomSheetAllMenu.show(supportFragmentManager, "bottomsheet")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/islamicapp/fragment/AllMenuFragment.kt
@@ -0,0 +1,33 @@
+package com.islamicapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.islamicapp.R
+import com.islamicapp.databinding.FragmentAllMenuBinding
+
+class AllMenuFragment : BottomSheetDialogFragment() {
+
+    private var _binding: FragmentAllMenuBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentAllMenuBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun getTheme(): Int {
+        return R.style.AppBottomSheetDialogTheme
+    }
+
+}
